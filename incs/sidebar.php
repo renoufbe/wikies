1bde50a33106829868a4e5bb07226eab59b355d7
renoufbe <be.renouf@laposte.net>
2017-11-20 15:50:23 +0100

    Add include files

---
+++ incs/sidebar.php
@@ -0,0 +1,19 @@
+<?php include '../incs/header.php' ;>
+    <!-- Page Content -->
+    <div class="container">
+
+      
+
+        <div class="col-lg-3">
+
+          <h1 class="my-4">Shop Name</h1>
+          <div class="list-group">
+            <a href="#" class="list-group-item">Category 1</a>
+            <a href="#" class="list-group-item">Category 2</a>
+            <a href="#" class="list-group-item">Category 3</a>
+          </div>
+
+        </div>
+        <!-- /.col-lg-3 -->
+
+        <div class="col-lg-9 mt-4">
